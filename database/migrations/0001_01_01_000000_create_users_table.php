93c3e8e8c58fa725b53ba9b693aa53497829b7ca
NgocAn215 <phamngocan201205@icloud.com>
2025-06-07 21:51:08 +0700

    Thêm các bảng migration trong database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('password', 255);
+            $table->string('phone', 20);
+            $table->foreignId('role_id')->constrained('roles');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps(); // created_at và updated_at
+            $table->softDeletes(); // deleted_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
