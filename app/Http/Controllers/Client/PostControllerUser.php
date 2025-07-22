3e63ae8bea563d593a8c45cf4aa82dc6664feed8
Htuan1911 <tuandhph51200@gmail.com>
2025-07-22 14:43:36 +0700

    Cập nhật giao diện blog, xóa ảnh cũ, thêm ảnh mới

--- app/Http/Controllers/Client/PostControllerUser.php
+++ app/Http/Controllers/Client/PostControllerUser.php
@@ -11,14 +11,14 @@ class PostControllerUser extends Controller
     public function index()
     {
         // Hiển thị danh sách bài viết (chỉ những bài đăng status = 1)
-        $posts = Post::where('status', 1)->orderBy('created_at', 'desc')->paginate(6);
+        $posts = Post::where('status', 2)->orderBy('created_at', 'desc')->paginate(6);
         return view('client.blog.index', compact('posts'));
     }
 
     public function show($slug)
     {
         // Hiển thị chi tiết bài viết theo slug
-        $post = Post::where('slug', $slug)->where('status', 1)->firstOrFail();
+        $post = Post::where('slug', $slug)->where('status', 2)->firstOrFail();
         return view('client.blog.show', compact('post'));
     }
 }
