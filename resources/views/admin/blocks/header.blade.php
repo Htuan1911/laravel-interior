d66f273deccf365937ad427ae3819ded7590c13c
Htuan1911 <tuandhph51200@gmail.com>
2025-08-08 22:15:57 +0700

    Cập nhật giao diện và logic orders

--- resources/views/admin/blocks/header.blade.php
+++ resources/views/admin/blocks/header.blade.php
@@ -2,9 +2,11 @@
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Admin Panel</a>
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-            <form method="POST" action="">
+            <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <button type="submit" class="nav-link btn btn-link text-white text-decoration-none p-0 m-0">Đăng xuất</button>
+                <button type="submit" class="nav-link btn btn-link text-white text-decoration-none p-0 m-0">
+                    Đăng xuất
+                </button>
             </form>
         </li>
     </ul>
